e3d1bf289fff80cf5384f5995a0c54ef1847735c
jjun0407 <67462814+Yongjjun@users.noreply.github.com>
2026-01-21 15:33:37 +0900

    3000을 2000으로 수정

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -5,7 +5,7 @@ test('네이버 접속 화면 보기', async ({ page }) => {
   await page.goto('https://www.naver.com');
   
   // 잠시 화면을 볼 수 있게 3초만 대기 (원래는 안 써도 되지만 확인용!)
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(2000);
   
   // 타이틀 확인
   await expect(page).toHaveTitle(/NAVER/);
