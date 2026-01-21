718fd214cd4704c7afcb917f36b74ae50d5b7966
Seungho8392 <dltmdghrj@naver.com>
2026-01-21 16:07:12 +0900

    112

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,12 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('네이버 접속 화면 보기', async ({ page }) => {
-  // 네이버로 이동
-  await page.goto('https://www.naver.com');
-  
-  // 잠시 화면을 볼 수 있게 3초만 대기 (원래는 안 써도 되지만 확인용!)
-  await page.waitForTimeout(2000);
-  
-  // 타이틀 확인
-  await expect(page).toHaveTitle(/NAVER/);
+
+  await page.goto('https://www.naver.com/');
+  await page.getByRole('combobox', { name: '검색어를 입력해 주세요' }).fill('이모티콘샵');
+  await page.getByRole('button', { name: '검색', exact: true }).click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.locator('a').filter({ hasText: /^카카오 이모티콘샵$/ }).click();
+  const page1 = await page1Promise;
 });
