f23a4c757d2960a2e4d1f25de22d02651eb9b405
Seungho8392 <dltmdghrj@naver.com>
2026-03-11 15:07:19 +0900

    ~~~

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,8 +13,9 @@ export default defineConfig({
     // 1. 뷰포트 및 브라우저 기본 설정
     viewport: { width: 2560, height: 1440 },
     headless: false,  // 💡 화면을 보려면 false로 두는 게 좋습니다! (기존 true였음)
+    // video: 'retain-on-failure',   // 실패 시 영상 저장!
     video: 'on',
-
+    screenshot: 'only-on-failure', // 실패 시 스크린샷 찰칵!
     // 2. 권한 설정
     permissions: ['notifications', 'geolocation'],
     geolocation: { longitude: 127.1086, latitude: 37.4012 },
